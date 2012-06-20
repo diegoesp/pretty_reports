df4df27317a3538958de007d96dac58998ea1e76
Rafael Chiti <rafaelchiti@gmail.com>
2012-06-20 20:25:36 -0300

    Rails app structure. Generated by 'rails new ./'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrettyReports::Application.config.secret_token = 'f1efbe688fc2e695027d1a0564eb7c69b9af77a14619e145ff781a6a6dfc5cacf0f9fa394338e24032e7fddf586dddffe8683e9cd59d31a3e701c7e45a346821'
