5da7d067c508f92755d42043ef5fc39f984e55c2
Rafael Chiti <rafaelchiti@gmail.com>
2012-08-04 16:24:29 -0300

    No funco lo de convertapi por un timeout. Despues lo vere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  #http_basic_authenticate_with :name => "user", :password => "lacasadelarte"
+  http_basic_authenticate_with :name => "user", :password => "lacasadelarte"
 
 end
