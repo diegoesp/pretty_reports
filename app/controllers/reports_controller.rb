80d0c2c00573a6d8c4b8db6ac03c589bb378cab3
Rafael Chiti <rafaelchiti@gmail.com>
2012-08-04 15:48:20 -0300

    Mas cambios para hacer pruebas con convertapi

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,7 +10,8 @@ class ReportsController < ApplicationController
 
   def download
     authenticated_url = report_url(params[:id]).sub 'http://', 'http://user:lacasadelarte@'
-
+    logger.debug ">>>>>>>>>>>>>>>>>>>"
+    logger.debug authenticated_url
     url = CONVERT_API_BASE_URL + "curl=#{authenticated_url}"
     resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
     send_data(resp.body, :filename => "Report.pdf", :type => "application/pdf")
