3ea60bf648d2a8dcf6a108d67d258b7fa5fff8a6
Rafael Chiti <rafaelchiti@gmail.com>
2012-08-04 15:26:16 -0300

    Modifique el controller para que le pegue a convert api con nuestros reportes de demo

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,7 +9,7 @@ class ReportsController < ApplicationController
   end
 
   def download
-    url = CONVERT_API_BASE_URL + 'curl=http://www.mozilla.org/en-US/firefox/14.0.1/releasenotes/'
+    url = CONVERT_API_BASE_URL + "curl=#{report_url(params[:id])}"
     resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
     send_data(resp.body, :filename => "Report.pdf", :type => "application/pdf")
   end
